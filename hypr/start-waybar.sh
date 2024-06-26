9ad7bd5db7e15a4807ae1744f2522b6dcf3c26bf
Trevor Martin <trevor.j.martin@gmail.com>
2024-06-25 22:19:26 -0400

    added hypr plugin config: hyprlock, hypridle; update paths

--- hypr/start-waybar.sh
+++ hypr/start-waybar.sh
@@ -2,6 +2,6 @@
 ps -ea |grep waybar|awk '{print $1}' | xargs kill -9 --
 
 #waybar -c ~/dotfiles/waybar/config.jsonc -s ~/dotfiles/waybar/style.css
-waybar -c ~/dotfiles/waybar/config.json -s ~/dotfiles/waybar/style.css
+waybar -c /etc/nixos/dotfiles/waybar/config.json -s /etc/nixos/dotfiles/waybar/style.css
 
 
